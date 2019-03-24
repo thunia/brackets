bf4e297f7af8d7ee3a10d024f2d13bb59c07b71b
Irina Kotova <irina.lushagina@gmail.com>
2019-03-24 11:07:57 +0300

    feat: brackets check function implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var stack = [],
+      brackets = {};
+
+  for (var j = 0; j < bracketsConfig.length; j++) {
+    brackets[bracketsConfig[j][0]] = bracketsConfig[j][1];
+  }
+
+  for (var i = 0; i < str.length; i++) {
+    if (i == 0) {
+      stack.push(str[i]);
+    }
+    else if (str[i] === brackets[stack[stack.length - 1]]) {
+      stack.pop();
+    }
+    else {
+      stack.push(str[i]);
+    }
+  }
+
+  return !stack.length;
 }
